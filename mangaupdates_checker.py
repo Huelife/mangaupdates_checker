ccfea687e932c91545b41d2440a1471bb081024a
Eric Meas <emmeas@gmail.com>
2019-12-25 14:41:54 -0800

    Update mangaupdates_checker.py
    added for loop to check working site and response

--- mangaupdates_checker.py
+++ mangaupdates_checker.py
@@ -7,3 +7,15 @@ from requests.exceptions import HTTPError
 
 link = 'https://www.mangaupdates.com/releases.html'
 chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+for url in [link]:
+  try:
+    response = requests.get(url)
+    response.raise_for_status()
+  except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+  except Exception as err:
+    print(f'Other error occurred: {err}')
+  else:
+    print('Success!')
+    webbrowser.get(chrome_loc).open_new_tab(link)  #open link if no errors
