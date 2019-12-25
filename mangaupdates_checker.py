16c86d4c6600b5cfba91cb4cd4f91829ee6940ee
Eric Meas <emmeas@gmail.com>
2019-12-25 11:57:33 -0800

    Update mangaupdates_checker.py
    added variables for links

--- mangaupdates_checker.py
+++ mangaupdates_checker.py
@@ -4,3 +4,6 @@ import webbrowser
 
 import requests
 from requests.exceptions import HTTPError
+
+link = 'https://www.mangaupdates.com/releases.html'
+chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
