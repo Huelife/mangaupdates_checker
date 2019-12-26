b73f5d6821c64dccb331ea64a887616e8ed5bbb0
Eric Meas <emmeas@gmail.com>
2019-12-26 00:26:26 -0800

    Update mangaupdates_checker.py
    added if/else statement to determine if new manga chapter is out

--- mangaupdates_checker.py
+++ mangaupdates_checker.py
@@ -12,14 +12,18 @@ chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 page = urllib.request.urlopen(link).read().decode('utf-8')
 match = re.findall("56<", page)
 
-for url in [link]:
-  try:
-    response = requests.get(url)
-    response.raise_for_status()
-  except HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')
-  except Exception as err:
-    print(f'Other error occurred: {err}')
-  else:
-    print('Success!')
-    webbrowser.get(chrome_loc).open_new_tab(link)  #open link if no errors
+if match is None or len(match) == 0: 
+  print('No new chapter...')
+else:
+  print('New chapter is out!')
+  for url in [link]:
+    try:
+      response = requests.get(url)
+      response.raise_for_status()
+    except HTTPError as http_err:
+      print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+      print(f'Other error occurred: {err}')
+    else:
+      print('Success!')
+      webbrowser.get(chrome_loc).open_new_tab(link)  #open link if no errors
