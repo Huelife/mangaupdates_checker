a25b53618c126a573ead01bf3ba71678cc49b733
Eric Meas <emmeas@gmail.com>
2019-12-29 10:25:47 -0800

    Update mangaupdates_checker.py
    removed verify_chapter function from Manga superclass and added it to all subclasses

--- mangaupdates_checker.py
+++ mangaupdates_checker.py
@@ -11,6 +11,18 @@ from requests.exceptions import HTTPError
 class Manga():
   def __init__(self,title):
     self.title = title
+
+#One Piece manga, current chapter
+class OnePiece(Manga):
+  def __init__(self,title,series,chapter):
+    self.manga_title = 'One Piece'
+    self.series = '33&stype=series'
+    self.chapter = '968<'
+    chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    link = 'https://www.mangaupdates.com/releases.html?search={}'.format(series)
+    page = urllib.request.urlopen(link).read().decode('utf-8')
+    match = re.findall(chapter, page)
+    super().__init__(title)
     
   #if new chapter == True, attempt to open webpage  
   def verify_chapter(self):
@@ -33,18 +45,6 @@ class Manga():
         time.sleep(1.5)
         webbrowser.get(chrome_loc).open_new_tab(link)
         #open link if no errors
-
-#One Piece manga, current chapter
-class OnePiece(Manga):
-  def __init__(self,title,series,chapter):
-    self.manga_title = 'One Piece'
-    self.series = '33&stype=series'
-    self.chapter = '968<'
-    chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-    link = 'https://www.mangaupdates.com/releases.html?search={}'.format(series)
-    page = urllib.request.urlopen(link).read().decode('utf-8')
-    match = re.findall(chapter, page)
-    super().__init__(title)
  
 #Vinland Saga manga, current chapter
 class VinlandSaga(Manga):
@@ -57,3 +57,25 @@ class VinlandSaga(Manga):
     page = urllib.request.urlopen(link).read().decode('utf-8')
     match = re.findall(chapter, page)
     super().__init__(title)
+    
+  #if new chapter == True, attempt to open webpage  
+  def verify_chapter(self):
+    if match is None or len(match) == 0:
+      print(manga_title+':')
+      print('No new chapter...')
+      print('')    
+    else:
+      print(manga_title+':')
+      try:
+        response = requests.get(link)
+        response.raise_for_status()
+      except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+      except Exception as err:
+        print(f'Other error occurred: {err}')
+      else:
+        print('New chapter is out!')
+        print('')
+        time.sleep(1.5)
+        webbrowser.get(chrome_loc).open_new_tab(link)
+        #open link if no errors
