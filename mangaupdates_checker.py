e3a2bc052d2e6c64205ee030addb661fd7920306
Eric Meas <emmeas@gmail.com>
2019-12-28 23:35:12 -0800

    Update mangaupdates_checker.py
    removed old code marked for deletion and added some comments

--- mangaupdates_checker.py
+++ mangaupdates_checker.py
@@ -8,6 +8,7 @@ import requests
 from requests.exceptions import HTTPError
 
 class Manga():
+  #master variables
   manga_title = ''
   series = ''
   chapter = ''
@@ -19,6 +20,7 @@ class Manga():
   def __init__(self,title):
     self.title = title
     
+  #if new chapter == True, attempt to open webpage  
   def verify_chapter(self):
     if match is None or len(match) == 0:
       print(self.title+':')
@@ -38,74 +40,19 @@ class Manga():
         print('')
         webbrowser.get(chrome_loc).open_new_tab(link)
         #open link if no errors
-    
+
+#One Piece manga, current chapter
 class OnePiece(Manga):
   def __init__(self,title,series,chapter):
     self.manga_title = 'One Piece'
     self.series = '33&stype=series'
     self.chapter = '968'
     super().__init__(title)
-    
+ 
+#Vinland Saga manga, current chapter
 class VinlandSaga(Manga):
   def __init__(self,title,series,chapter):
     self.manga_title = 'Vinland Saga'
     self.series = '1568&stype=series'
     self.chapter = '169'
     super().__init__(title)
-
-#variables for webbrowser, website link, reading webpage, and re
-#link_01 = 'https://www.mangaupdates.com/releases.html?search=33&stype=series'
-#link_02 = 'https://www.mangaupdates.com/releases.html?search=1568&stype=series'
-#page_01 = urllib.request.urlopen(link_01).read().decode('utf-8')
-#page_02 = urllib.request.urlopen(link_02).read().decode('utf-8')
-#match_01 = re.findall("967<", page_01)
-#match_02 = re.findall("169<", page_02)
-
-#grouping multiple links and matches in tuples
-#link = {
-#  0:link_01,
-#  1:link_02,
-#  }
-#match = {
-#  0:match_01,
-#  1:match_02,
-#  }
-
-#if new chapter == True, attempt to open webpage
-#for x in range(2):
- # if match[x] is None or len(match[x]) == 0:
-  #  if link[x] == link_01:
-   #   print('One Piece:')    
-    #elif link[x] == link_02:
-    #  print('Vinland Saga:')
-    #print('No new chapter...')
-    #print('')
-  #else:
-#    if link[x] == link_01:
- #     print('One Piece:')
-  #    try:
-   #     response = requests.get(link[x])
-    #    response.raise_for_status()
-     # except HTTPError as http_err:
-      #  print(f'HTTP error occurred: {http_err}')
-      #except Exception as err:
- #       print(f'Other error occurred: {err}')
-  #    else:
-   #     print('New chapter is out!')
-    #    print('')
-     #   webbrowser.get(chrome_loc).open_new_tab(link[x])
-        #open link if no errors
- #   elif link[x] == link_02:
-  #    print('Vinland Saga:')
-   #   try:
-#        response = requests.get(link[x])
- #       response.raise_for_status()
-  #    except HTTPError as http_err:
-   #     print(f'HTTP error occurred: {http_err}')
-    #  except Exception as err:
-     #   print(f'Other error occurred: {err}')
-#      else:
- #       print('New chapter is out!')
-  #      print('')
-   #     webbrowser.get(chrome_loc).open_new_tab(link[x])
-        #open link if no errors
