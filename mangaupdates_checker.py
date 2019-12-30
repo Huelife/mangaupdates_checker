e6a5a7027d11bb4c51701a6b26e31660bf1063d6
Eric Meas <emmeas@gmail.com>
2019-12-29 22:07:19 -0800

    Update mangaupdates_checker.py
    added D.Gray-man manga subclass

--- mangaupdates_checker.py
+++ mangaupdates_checker.py
@@ -83,3 +83,39 @@ class VinlandSaga(Manga):
         time.sleep(1.5)
         webbrowser.get(self.chrome_loc).open_new_tab(self.link)
         #open link if no errors
+
+#D.Gray-man manga, new chapter
+class DGrayman(Manga):
+  def __init__(self,title):
+    self.manga_title = 'D.Gray-man'
+    self.series = '95&stype=series'
+    self.chapter = '235<'
+    self.chrome_loc = ('C:/Program Files (x86)/Google/Chrome/Application/'
+                       'chrome.exe %s')
+    self.link = ('https://www.mangaupdates.com/releases.html?search={}'
+                 .format(self.series))
+    self.page = urllib.request.urlopen(self.link).read().decode('utf-8')
+    self.match = re.findall(self.chapter, self.page)
+    super().__init__(title)
+
+  #if new chapter == True, attempt to open webpage  
+  def verify_chapter(self):
+    if self.match is None or len(self.match) == 0:
+      print(self.manga_title+':')
+      print('No new chapter...')
+      print('')    
+    else:
+      print(self.manga_title+':')
+      try:
+        response = requests.get(self.link)
+        response.raise_for_status()
+      except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+      except Exception as err:
+        print(f'Other error occurred: {err}')
+      else:
+        print('New chapter is out!')
+        print('')
+        time.sleep(1.5)
+        webbrowser.get(self.chrome_loc).open_new_tab(self.link)
+        #open link if no errors
