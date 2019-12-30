2473a67ee14dc34e8973f106143527ae587b1860
Eric Meas <emmeas@gmail.com>
2019-12-29 22:11:48 -0800

    Update mangaupdates_checker.py
    added OnePunchMan subclass

--- mangaupdates_checker.py
+++ mangaupdates_checker.py
@@ -119,3 +119,39 @@ class DGrayman(Manga):
         time.sleep(1.5)
         webbrowser.get(self.chrome_loc).open_new_tab(self.link)
         #open link if no errors
+
+#One Punch-Man manga, new chapter
+class OnePunchMan(Manga):
+  def __init__(self,title):
+    self.manga_title = 'One Punch-Man'
+    self.series = '80345&stype=series'
+    self.chapter = '125<'
+    self.chrome_loc = ('C:/Program Files (x86)/Google/Chrome/Application/'
+                       'chrome.exe %s')
+    self.link = ('https://www.mangaupdates.com/releases.html?search={}'
+                 .format(self.series))
+    self.page = urllib.request.urlopen(self.link).read().decode('utf-8')
+    self.match = re.findall(self.chapter, self.page)
+    super().__init__(title)
+
+  #if new chapter == True, attempt to open webpage  
+  def verify_chapter(self):
+    if self.match is None or len(self.match) == 0:
+      print(self.manga_title+':')
+      print('No new chapter...')
+      print('')    
+    else:
+      print(self.manga_title+':')
+      try:
+        response = requests.get(self.link)
+        response.raise_for_status()
+      except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+      except Exception as err:
+        print(f'Other error occurred: {err}')
+      else:
+        print('New chapter is out!')
+        print('')
+        time.sleep(1.5)
+        webbrowser.get(self.chrome_loc).open_new_tab(self.link)
+        #open link if no errors
